ebc0c80ba82771fee65c16272b89c9204a52e92e
Fabio Salles <65322717+fs2050@users.noreply.github.com>
2021-10-13 11:20:42 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
- URL: 'https://my-json-server.typicode.com/fs2050/backangular/products',
+ URL: 'https://my-json-server.typicode.com/fs2050/backangular/db',
 
 
 
